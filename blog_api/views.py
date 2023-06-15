54e7dee3db1aea0966c58764d73d68e681470b30
AbduraimovErlan <96403251+AbduraimovErlan@users.noreply.github.com>
2023-06-14 21:49:46 -0400

    Initial commit

---
+++ blog_api/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from rest_framework import generics
+from blog.models import Post
+from .serializers import PostSerializer
+
+
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.postobjects.all()
+    serializer_class = PostSerializer
+
+    pass
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    pass
